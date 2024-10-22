2bb1c04500305c951e3829923c858df9f2865908
rakesh <rakesh@falconsystem.com>
2024-10-23 04:02:22 +0530

    commit

--- app.js
+++ app.js
@@ -287,5 +287,5 @@ app.use('/api/auth', authRoutes);
 app.use('/api', fileRoutes);
 
 app.listen(4000, () => {
-    console.log('Server started on port 3000');
+    console.log('Server started on port 4000');
 });
